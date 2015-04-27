aaf30e132f89030e5607dc18a707fdeeae51b6c4
Janson Kang <temasek71@gmail.com>
2015-04-27 20:16:14 +0800

    grouper: Remove duplicate and clean up

--- device.mk
+++ device.mk
@@ -14,21 +14,18 @@
 # limitations under the License.
 #
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.carrier=wifi-only
-
 PRODUCT_COPY_FILES := \
     device/asus/grouper/fstab.grouper:root/fstab.grouper \
     device/asus/grouper/init.grouper.rc:root/init.grouper.rc
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.carrier=wifi-only
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/asus/grouper/overlay
 
 # the actual meat of the device-specific product definition
 $(call inherit-product, device/asus/grouper/device-common.mk)
 
 # inherit from the non-open-source side, if present
 $(call inherit-product-if-exists, vendor/asus/grouper/device-vendor.mk)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carrier=wifi-only
-
-DEVICE_PACKAGE_OVERLAYS := \
-    device/asus/grouper/overlay
